d0ea88eefb1cffce44e31614b2e7fda256ccc5c9
Alex12Rodrigues <alex.rodoliveira2@gmail.com>
2024-09-05 21:19:45 -0300

    Realizando o cadastrar livro

---
+++ pw3-alex-app-libri/src/components/forms/Button.jsx
@@ -0,0 +1,21 @@
+import styles from './Button.module.css'
+
+function Button ({rotulo}){
+
+    return(
+
+        <div className={styles.button}>
+
+            <button>{rotulo}
+
+            </button>
+        </div>
+    )
+}
+
+
+export default Button
+
+
+
+
