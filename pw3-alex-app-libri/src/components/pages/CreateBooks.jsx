ed33786711955fb2be6752189f1953663d563b64
Alex12Rodrigues <alex.rodoliveira2@gmail.com>
2024-09-19 22:27:32 -0300

    Comunicando com back

--- pw3-alex-app-libri/src/components/pages/CreateBooks.jsx
+++ pw3-alex-app-libri/src/components/pages/CreateBooks.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from './CreateBooks.module.css'
 
 import Input from "../forms/Input";
@@ -6,8 +6,32 @@ import Select from "../forms/Select";
 import Button from "../forms/Button";
 
 
-
 const CreateBooks = () => {
+
+    useEffect(() => {
+        fetch('http://localhost:5000/listagemCateorias', {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*",
+          },
+        }).then(
+            (resp)=>
+                // console.log('RESPOSTA: '+ resp)
+                resp.json()
+            
+        ).then(
+            (data)=>{
+                console.log('DATA: ' + data.data[0].nome_categoria)
+            }
+        ).catch(
+            (error)=>{
+                console.log(error)
+            }
+        )
+    },[]);
+
 
     return (
 
